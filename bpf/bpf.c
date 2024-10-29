769289a3f5039ee81100e5f39888fb7a59a50178
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2024-10-30 07:57:29 +0900

    Use early return

--- bpf/bpf.c
+++ bpf/bpf.c
@@ -30,6 +30,11 @@ int do_test_data(struct __sk_buff *skb) {
 
   ulogf("Packet: src=%pI6, dst=%pI6", (u64)&ip6h->saddr, (u64)&ip6h->daddr);
 
+  if (ip6h->nexthdr != IPPROTO_ROUTING) return BPF_OK;  // 43: Routing Header
+  struct ipv6_sr_hdr *sr_hdr = (struct ipv6_sr_hdr *)(ip6h + 1);
+  if ((void *)(sr_hdr + 1) > data_end) return BPF_DROP;
+  if (sr_hdr->type != 4) return BPF_DROP;  // 4: Segment Routing Header
+
   u16 func = SID_FUNC(&ip6h->daddr);
   u64 arg = SID_ARG(&ip6h->daddr);
 
@@ -46,68 +51,55 @@ int do_test_data(struct __sk_buff *skb) {
         "nic_index=%u, bps=%u",
         func, num_skip, metrics, comparator, nic_index, bps);
 
-    // check if payload has SRv6 header
-    if (ip6h->nexthdr == IPPROTO_ROUTING) {
-      struct ipv6_sr_hdr *sr_hdr = (struct ipv6_sr_hdr *)(ip6h + 1);
-
-      if ((void *)(sr_hdr + 1) > data_end) {
-        bpf_printk("packet truncated");
-        return BPF_DROP;
-      }
-      // check if the type is SRH
-      if (sr_hdr->type != 4) return BPF_DROP;
-
-      bool match = false;
-      if (metrics == 0) {
-        u64 key = nic_index;
-        u64 *matrics_value = bpf_map_lookup_elem(&tx_bytes_per_sec, &key);
-        if (!matrics_value) return BPF_DROP;
-        if (comparator == 0)
-          match = (*matrics_value == bps);
-        else if (comparator == 1)
-          match = (*matrics_value > bps);
-        else if (comparator == 2)
-          match = (*matrics_value < bps);
-        ulogf("match=%d, matrics_value=%llu, bps=%u", match, *matrics_value,
-              bps);
-      }
-
-      if (sr_hdr->segments_left == 0) return BPF_DROP;
-      u8 new_segments_left = sr_hdr->segments_left - 1;
-      bool should_decap = false;
-      if (match) {
-        if (new_segments_left >= num_skip) {
-          new_segments_left -= num_skip;
-        } else {
-          should_decap = true;
-        }
-      };
-
-      if (should_decap) {
-        // // decap the SRH
-        // if ((void *)(sr_hdr->segments + 1) > data_end) return BPF_DROP;
-        // // struct in6_addr localhost = {.in6_u.u6_addr32 = {0, 0, 0, 1}};
-        // // fc00:a:12:0:0001::
-        // struct in6_addr seg6_end = {.in6_u.u6_addr8 = {
-        //                                 // clang-format off
-        //   0xfc, 0x00, 0x00, 0x0a, 0x00, 0x00, 0x00, 0x12,
-        //   0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        //                                 // clang-format on
-        //                             }};
-        // ip6h->daddr = seg6_end;
-        // sr_hdr->segments[0] = seg6_end;
-        // sr_hdr->segments_left = 0;
-        return BPF_DROP;
-      } else {
-        struct in6_addr *new_dst_ptr = sr_hdr->segments + new_segments_left;
-        if ((void *)(new_dst_ptr + 1) > data_end) return BPF_DROP;
-        ip6h->daddr = *new_dst_ptr;
-        sr_hdr->segments_left = new_segments_left;
-      }
-      ulogf("Dst updated: new_dst=%pI6", (u64)&ip6h->daddr);
-
-      return BPF_LWT_REROUTE;
+    bool match = false;
+    if (metrics == 0) {
+      u64 key = nic_index;
+      u64 *matrics_value = bpf_map_lookup_elem(&tx_bytes_per_sec, &key);
+      if (!matrics_value) return BPF_DROP;
+      if (comparator == 0)
+        match = (*matrics_value == bps);
+      else if (comparator == 1)
+        match = (*matrics_value > bps);
+      else if (comparator == 2)
+        match = (*matrics_value < bps);
+      ulogf("match=%d, matrics_value=%llu, bps=%u", match, *matrics_value, bps);
     }
+
+    if (sr_hdr->segments_left == 0) return BPF_DROP;
+    u8 new_segments_left = sr_hdr->segments_left - 1;
+    bool should_decap = false;
+    if (match) {
+      if (new_segments_left >= num_skip) {
+        new_segments_left -= num_skip;
+      } else {
+        should_decap = true;
+      }
+    };
+
+    if (should_decap) {
+      // // decap the SRH
+      // if ((void *)(sr_hdr->segments + 1) > data_end) return BPF_DROP;
+      // // struct in6_addr localhost = {.in6_u.u6_addr32 = {0, 0, 0, 1}};
+      // // fc00:a:12:0:0001::
+      // struct in6_addr seg6_end = {.in6_u.u6_addr8 = {
+      //                                 // clang-format off
+      //   0xfc, 0x00, 0x00, 0x0a, 0x00, 0x00, 0x00, 0x12,
+      //   0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      //                                 // clang-format on
+      //                             }};
+      // ip6h->daddr = seg6_end;
+      // sr_hdr->segments[0] = seg6_end;
+      // sr_hdr->segments_left = 0;
+      return BPF_DROP;
+    } else {
+      struct in6_addr *new_dst_ptr = sr_hdr->segments + new_segments_left;
+      if ((void *)(new_dst_ptr + 1) > data_end) return BPF_DROP;
+      ip6h->daddr = *new_dst_ptr;
+      sr_hdr->segments_left = new_segments_left;
+    }
+    ulogf("Dst updated: new_dst=%pI6", (u64)&ip6h->daddr);
+
+    return BPF_LWT_REROUTE;
   }
 
   return BPF_OK;
