9a51f74ab3c19cf87e3d5b1cda3f9d4c1af79258
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2024-10-29 16:17:22 +0900

    ファイルを分割

--- bpf/bpf.c
+++ bpf/bpf.c
@@ -7,39 +7,15 @@
 #include <bpf/bpf_helpers.h>
 // clang-format on
 
+#include "ulogf.h"
+#include "utils.h"
+
 struct {
   __uint(type, BPF_MAP_TYPE_ARRAY);
   __type(key, __u32);
   __type(value, __u64);
   __uint(max_entries, 1);
 } tx_bytes_per_sec SEC(".maps");
-
-struct {
-  __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
-  __uint(max_entries, 128);
-  __uint(key_size, sizeof(__u32));
-  __uint(value_size, sizeof(__u32));
-} log_entries SEC(".maps");
-
-// IPv6 Routing header
-#define IPPROTO_ROUTING 43
-#define ntohll(x) (((u64)bpf_ntohl(x)) << 32) + bpf_ntohl(x >> 32)
-#define SID_FUNC(x) bpf_ntohs(*((u16 *)(x) + 4))
-#define SID_ARG(x) ntohll(*((u64 *)(x) + 1)) & 0x0000FFFFFFFFFFFF
-
-// ユーザー空間にログを出力するための関数
-// Note: 引数はu64型にキャストされている必要がある
-#define ulogf(fmt, args...)                                                    \
-  ({                                                                           \
-    static const char _fmt[] = fmt;                                            \
-    static char _buf[256];                                                     \
-    u64 _args[___bpf_narg(args)];                                              \
-    ___bpf_fill(_args, args);                                                  \
-    int _len = bpf_snprintf(_buf, sizeof(_buf), _fmt, _args, sizeof(_args));   \
-    if (_len < sizeof(_buf)) {                                                 \
-      bpf_perf_event_output(skb, &log_entries, BPF_F_CURRENT_CPU, _buf, _len); \
-    }                                                                          \
-  })
 
 SEC("lwt_xmit/test_data")
 int do_test_data(struct __sk_buff *skb) {
