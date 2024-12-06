bf1d383ee963ae1cbf59e18f6b41c2905795a89b
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2024-12-07 01:53:33 +0900

    update binany name

--- scripts/insert_ebpf_routes.sh
+++ scripts/insert_ebpf_routes.sh
@@ -1,5 +1,5 @@
-ip netns exec ns2 ./build/ebpf-test fc00:a:21::/64 fc00:a:: 10ms &
-ip netns exec ns5 ./build/ebpf-test fc00:d:53::/64 fc00:d:: 10ms &
-ip netns exec ns5 ./build/ebpf-test fc00:e:54::/64 fc00:e:: 10ms &
+ip netns exec ns2 ./build/lb-proposal fc00:a:21::/64 fc00:a:: 10ms &
+ip netns exec ns5 ./build/lb-proposal fc00:d:53::/64 fc00:d:: 10ms &
+ip netns exec ns5 ./build/lb-proposal fc00:e:54::/64 fc00:e:: 10ms &
 
 wait
